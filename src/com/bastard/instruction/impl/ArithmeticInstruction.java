38891f5a5d4332a613f3757b7c214685861ad089
Shawn Davies <sodxeh@gmail.com>
2013-11-05 11:17:41 -0500

    Added a few new instruction impls.

---
+++ src/com/bastard/instruction/impl/ArithmeticInstruction.java
@@ -0,0 +1,34 @@
+package com.bastard.instruction.impl;
+
+import java.nio.ByteBuffer;
+
+import com.bastard.instruction.Instruction;
+import com.bastard.instruction.Opcode;
+
+/**
+ * An instruction that executes math operations on the stack.
+ * @author Shawn Davies<sodxeh@gmail.com>
+ *
+ */
+public class ArithmeticInstruction extends Instruction {
+
+	public ArithmeticInstruction(int opcode) {
+		super(opcode);
+	}
+
+	@Override
+	public Instruction read(ByteBuffer code) {
+		System.out.println("\t\t\t\t" + toString());
+		return this;
+	}
+
+	@Override
+	public String toString() {
+		Opcode opcode = Opcode.valueOf(getOpcode() & 0xFF);
+		if (opcode == null) {
+			return "ArithmeticInstruction[op="+(getOpcode() & 0xFF)+", null]";
+		}
+		return "ArithmeticInstruction[op=" + opcode.toString() + "]";
+	}
+
+}
